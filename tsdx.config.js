14ed8428a0d112de0f5d55613334a0e97b9547a4
Mordechai Meisels <mordechai@codaworks.com>
2023-09-28 11:41:43 -0400

    Add Next.js 'use client' directive

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,9 +1,11 @@
 const postcss = require('rollup-plugin-postcss')
+const banner = require('rollup-plugin-banner2')
 
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
-      postcss()
+      postcss(),
+      banner(() => '"use client";\n')
     )
     return config
   }
